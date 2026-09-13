26be6093f3581cd4d8cc73b722eb487586007e2f
Lmslickolster <lmslickolster@gmail.com>
2026-09-13 18:10:31 +1200

    Use current Fabric block entity renderer registration

--- src/client/java/site/olsterstudios/vinyldiscs/VinylDiscsClient.java
+++ src/client/java/site/olsterstudios/vinyldiscs/VinylDiscsClient.java
@@ -1,12 +1,12 @@
 package site.olsterstudios.vinyldiscs;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererFactories;
 import net.minecraft.block.entity.BlockEntityType;
 
 public final class VinylDiscsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        BlockEntityRendererRegistry.register(BlockEntityType.JUKEBOX, VinylJukeboxRenderer::new);
+        BlockEntityRendererFactories.register(BlockEntityType.JUKEBOX, VinylJukeboxRenderer::new);
     }
 }
